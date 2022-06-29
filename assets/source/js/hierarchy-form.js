029c2c867b16e29c1e7c6935d93df1d411e1774a
Kira Penyaeva <k.penyaeva@ack-it.online>
2022-06-29 11:50:25 +0300

    small margins, tabs id,  enable checkboxes in change modal

--- assets/source/js/hierarchy-form.js
+++ assets/source/js/hierarchy-form.js
@@ -3,24 +3,38 @@
  */
 
 document.addEventListener("DOMContentLoaded", function (event) {
-    const scopes = document.querySelectorAll('.hierarchy__scope');
 
-    if (scopes) {
-        scopes.forEach(scope => scope.addEventListener('click', function () {
-            if (scope.classList.contains('hierarchy__scope--parent')) {
-                let parent = scope.parentNode;
-                //console.log(parent);
-                const parentInput = scope.querySelector('input');
-                let childrenInputs = parent.querySelectorAll('.hierarchy__child-scopes .js-checkbox-scope');
-                // console.log(childrenInputs);
-                if (parentInput.checked === true && childrenInputs) {
-                    childrenInputs.forEach(childInput => childInput.checked = true);
-                }
-                if (parentInput.checked === false && childrenInputs) {
-                    childrenInputs.forEach(childInput => childInput.checked = false);
-                }
-            }
-        }))
+    const changeRoleBtn = document.querySelector('.btn-group [data-action="edit"]');
+   // console.log(changeRoleBtn);
+
+    if(changeRoleBtn) {
+        changeRoleBtn.addEventListener('click', manipulateCheckboxes);
     }
+
+    function manipulateCheckboxes(){
+        setTimeout(() => {
+            const scopes = document.querySelectorAll('.hierarchy__scope');
+            if (scopes) {
+                scopes.forEach(scope => scope.addEventListener('click', function () {
+                    if (scope.classList.contains('hierarchy__scope--parent')) {
+                        let parent = scope.parentNode;
+                        //console.log(parent);
+                        const parentInput = scope.querySelector('input');
+                        let childrenInputs = parent.querySelectorAll('.hierarchy__child-scopes .js-checkbox-scope');
+                        // console.log(childrenInputs);
+                        if (parentInput.checked === true && childrenInputs) {
+                            childrenInputs.forEach(childInput => childInput.checked = true);
+                        }
+                        if (parentInput.checked === false && childrenInputs) {
+                            childrenInputs.forEach(childInput => childInput.checked = false);
+                        }
+                    }
+                }))
+            }
+        }, 1000);
+    }
+
+    manipulateCheckboxes();
+
 })
 
