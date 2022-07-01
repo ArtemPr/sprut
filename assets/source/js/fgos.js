f6d8457bcd18eabe8e804216830a07d53ce148d6
Kira Penyaeva <k.penyaeva@ack-it.online>
2022-07-01 10:21:32 +0300

    one btn to add new fgos fields

--- assets/source/js/fgos.js
+++ assets/source/js/fgos.js
@@ -13,16 +13,16 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     function createFgosItem() {
         event.preventDefault();
-        console.log('addFgosBtn click');
+    //    console.log('addFgosBtn click');
         let fgosClone = fgosItem.cloneNode(true);
         let input = fgosClone.querySelector('.form-control');
         let datalist = fgosClone.querySelector('.fgos-datalist');
-        console.log(input);
-        console.log(datalist);
+        // console.log(input);
+        // console.log(datalist);
         input.setAttribute('list', `datalistOptions${datalistId}`);
         datalist.setAttribute('id', `datalistOptions${datalistId}`);
-        let cloneAddBtn = fgosClone.querySelector('.add-fgos');
-        cloneAddBtn.addEventListener('click', createFgosItem);
+    //    let cloneAddBtn = fgosClone.querySelector('.add-fgos');
+    //    cloneAddBtn.addEventListener('click', createFgosItem);
         let allFgosItems = document.querySelectorAll('.fgos-item');
         if(allFgosItems.length > 1) {
             allFgosItems[allFgosItems.length -1].after(fgosClone);
