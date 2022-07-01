a3046e84efe55def0c55c1dd48bb57b578c6f3e2
Kira Penyaeva <k.penyaeva@ack-it.online>
2022-07-01 15:27:51 +0300

    select with arrow, levels for checkboxes

--- assets/source/js/dual-select.js
+++ assets/source/js/dual-select.js
@@ -1,67 +1,70 @@
 /*
  * Created AptPr <prudishew@yandex.ru> 2022.
  */
-document.addEventListener("DOMContentLoaded", function (event) {
-    const managingDepartmentsModals = document.querySelectorAll(
-        '[data-bs-target="#managingDepartmentsModal"]'
-    );
-
-    //console.log(managingDepartmentsModal.textContent);
-
-    const dualItems = document.querySelectorAll(".dual-listbox__item");
-    const leftPanel = document.querySelector(".dual-controls-select");
-    const rightPanel = document.querySelector(".dual-listbox__selected");
-    const addOneBtn = document.querySelector('[name="add_one"]');
-    const addAllBtn = document.querySelector('[name="add_all"]');
-    const removeOneBtn = document.querySelector('[name="remove_one"]');
-    const removeAllBtn = document.querySelector('[name="remove_all"]');
-
-    if (addOneBtn && leftPanel && rightPanel && dualItems) {
-        addOneBtn.addEventListener("click", addOne);
-    }
-
-    function addOne() {
-        console.log("addOne");
-        leftPanel.removeChild(this);
-        rightPanel.appendChild(this);
-    }
-
-    function toggleSelected() {
-        dualItems.forEach((dualItem) =>
-            dualItem.classList.remove("dual-listbox__item--selected")
-        );
-        this.classList.toggle("dual-listbox__item--selected");
-    }
-
-    dualItems.forEach((dualItem) => {
-        dualItem.addEventListener("click", toggleSelected);
-    });
-
-    // if (managingDepartmentsModals) {
-    //     managingDepartmentsModals.forEach((modalBtn) => {
-    //         modalBtn.addEventListener("click", function () {
-    //             console.log("click managingDepartmentsModal");
-    //         });
-    //     });
-    //     //  console.log('managingDepartmentsModal is')
-    //     managingDepartmentsModal.addEventListener("click", function () {
-    //         console.log("click managingDepartmentsModal");
-    //         // if (addOneBtn && leftPanel && rightPanel && dualItems) {
-    //         //     console.log('all, add f')
-    //         //     addOneBtn.addEventListener("click", addOne);
-    //         // }
-
-    //         // function toggleSelected(){
-    //         //     dualItems.forEach(dualItem=>dualItem.classList.remove('dual-listbox__item--selected'));
-    //         //     this.classList.toggle('dual-listbox__item--selected');
-    //         // }
-
-    //         // if (dualItems && leftPanel && rightPanel) {
-    //         //     dualItems.forEach((dualItem) =>
-    //         //         dualItem.addEventListener("dblclick", addOne)
-    //         //     );
-    //         //     //   dualItems.forEach(dualItem=>dualItem.addEventListener('click', toggleSelected));
-    //     });
-    //     //   });
-    // }
-});
+// временно элементов нет
+// document.addEventListener("DOMContentLoaded", function (event) {
+//     const managingDepartmentsModals = document.querySelectorAll(
+//         '[data-bs-target="#managingDepartmentsModal"]'
+//     );
+//
+//     //console.log(managingDepartmentsModal.textContent);
+//
+//     const dualItems = document.querySelectorAll(".dual-listbox__item");
+//     const leftPanel = document.querySelector(".dual-controls-select");
+//     const rightPanel = document.querySelector(".dual-listbox__selected");
+//     const addOneBtn = document.querySelector('[name="add_one"]');
+//     const addAllBtn = document.querySelector('[name="add_all"]');
+//     const removeOneBtn = document.querySelector('[name="remove_one"]');
+//     const removeAllBtn = document.querySelector('[name="remove_all"]');
+//
+//     console.log(addOneBtn);
+//
+//     if (addOneBtn && leftPanel && rightPanel && dualItems) {
+//         addOneBtn.addEventListener("click", addOne);
+//     }
+//
+//     function addOne() {
+//         console.log("addOne");
+//         leftPanel.removeChild(this);
+//         rightPanel.appendChild(this);
+//     }
+//
+//     function toggleSelected() {
+//         dualItems.forEach((dualItem) =>
+//             dualItem.classList.remove("dual-listbox__item--selected")
+//         );
+//         this.classList.toggle("dual-listbox__item--selected");
+//     }
+//
+//     dualItems.forEach((dualItem) => {
+//         dualItem.addEventListener("click", toggleSelected);
+//     });
+//
+//     // if (managingDepartmentsModals) {
+//     //     managingDepartmentsModals.forEach((modalBtn) => {
+//     //         modalBtn.addEventListener("click", function () {
+//     //             console.log("click managingDepartmentsModal");
+//     //         });
+//     //     });
+//     //     //  console.log('managingDepartmentsModal is')
+//     //     managingDepartmentsModal.addEventListener("click", function () {
+//     //         console.log("click managingDepartmentsModal");
+//     //         // if (addOneBtn && leftPanel && rightPanel && dualItems) {
+//     //         //     console.log('all, add f')
+//     //         //     addOneBtn.addEventListener("click", addOne);
+//     //         // }
+//
+//     //         // function toggleSelected(){
+//     //         //     dualItems.forEach(dualItem=>dualItem.classList.remove('dual-listbox__item--selected'));
+//     //         //     this.classList.toggle('dual-listbox__item--selected');
+//     //         // }
+//
+//     //         // if (dualItems && leftPanel && rightPanel) {
+//     //         //     dualItems.forEach((dualItem) =>
+//     //         //         dualItem.addEventListener("dblclick", addOne)
+//     //         //     );
+//     //         //     //   dualItems.forEach(dualItem=>dualItem.addEventListener('click', toggleSelected));
+//     //     });
+//     //     //   });
+//     // }
+// });
--- assets/source/js/hierarchy-form.js
+++ assets/source/js/hierarchy-form.js
@@ -3,38 +3,59 @@
  */
 
 document.addEventListener("DOMContentLoaded", function (event) {
+    const changeRoleBtn = document.querySelector(
+        '.btn-group [data-action="edit"]'
+    );
+    console.log(changeRoleBtn);
 
-    const changeRoleBtn = document.querySelector('.btn-group [data-action="edit"]');
-   // console.log(changeRoleBtn);
-
-    if(changeRoleBtn) {
-        changeRoleBtn.addEventListener('click', manipulateCheckboxes);
+    if (changeRoleBtn) {
+        changeRoleBtn.addEventListener("click", manipulateCheckboxes);
     }
 
-    function manipulateCheckboxes(){
+    function manipulateCheckboxes() {
         setTimeout(() => {
-            const scopes = document.querySelectorAll('.hierarchy__scope');
+            const scopes = document.querySelectorAll(".hierarchy__scope");
             if (scopes) {
-                scopes.forEach(scope => scope.addEventListener('click', function () {
-                    if (scope.classList.contains('hierarchy__scope--parent')) {
-                        let parent = scope.parentNode;
-                        //console.log(parent);
-                        const parentInput = scope.querySelector('input');
-                        let childrenInputs = parent.querySelectorAll('.hierarchy__child-scopes .js-checkbox-scope');
-                        // console.log(childrenInputs);
-                        if (parentInput.checked === true && childrenInputs) {
-                            childrenInputs.forEach(childInput => childInput.checked = true);
+                scopes.forEach((scope) =>
+                    scope.addEventListener("click", function () {
+                        console.log("this ", this);
+                        console.log(
+                            "scope.parentNode.parentNode ",
+                            scope.parentNode.parentNode
+                        );
+                        if (
+                            scope.classList.contains("hierarchy__scope--parent")
+                        ) {
+                            let parent = scope.parentNode;
+                            //console.log(parent);
+                            const parentInput = scope.querySelector("input");
+                            parentInput.style.outline = "2px dotted red";
+                            let childrenInputs = parent.querySelectorAll(
+                                ".hierarchy__child-scopes .js-checkbox-scope"
+                            );
+                            // console.log(childrenInputs);
+                            if (
+                                parentInput.checked === true &&
+                                childrenInputs
+                            ) {
+                                childrenInputs.forEach(
+                                    (childInput) => (childInput.checked = true)
+                                );
+                            }
+                            if (
+                                parentInput.checked === false &&
+                                childrenInputs
+                            ) {
+                                childrenInputs.forEach(
+                                    (childInput) => (childInput.checked = false)
+                                );
+                            }
                         }
-                        if (parentInput.checked === false && childrenInputs) {
-                            childrenInputs.forEach(childInput => childInput.checked = false);
-                        }
-                    }
-                }))
+                    })
+                );
             }
         }, 1000);
     }
 
     manipulateCheckboxes();
-
-})
-
+});
