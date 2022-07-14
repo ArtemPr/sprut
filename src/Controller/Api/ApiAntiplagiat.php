e181c324066cd879ae43d18ddbe4a63e67e3effb
admhome <admhome@ya.ru>
2022-07-14 14:11:43 +0300

    SPR-146 modify behavior of unique discipline

--- src/Controller/Api/ApiAntiplagiat.php
+++ src/Controller/Api/ApiAntiplagiat.php
@@ -104,7 +104,7 @@ class ApiAntiplagiat extends AbstractController
 
         $discipline = null;
 
-        if (!empty($data['discipline'])) {
+        if (empty($data['unique_discipline']) && !empty($data['discipline'])) {
             $discipline = $this->doctrine->getRepository(Discipline::class)->find($data['discipline']);
         }
 
