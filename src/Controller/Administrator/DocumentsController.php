65843edb1e24adcdfae4fc62ba6a1a27384331f8
Kira Penyaeva <k.penyaeva@ack-it.online>
2022-07-21 15:02:16 +0300

    documents_templates with 2 test rows

--- src/Controller/Administrator/DocumentsController.php
+++ src/Controller/Administrator/DocumentsController.php
@@ -29,13 +29,98 @@ class DocumentsController extends BaseController implements BaseInterface
 
         return $this->render('/administrator/document_templates/index.html.twig', [
             'controller' => 'document_templates',
-            'auth' => $auth
+            'auth' => $auth,
+            'table' => $this->setTable(),
+            // тестовые данные
+            'data' => [[
+                'active' => true,
+                'id' => '1',
+                'name' => 'Name',
+                'link' => 'http://axiomabio.com/pdf/test.pdf',
+                'comment' => 'lorem ipsum',
+                'date_create' => '20.12.2021',
+                'author' => 'Вася Иванов'
+            ],
+                [
+                    'active' => false,
+                    'id' => '2',
+                    'name' => 'Name2',
+                    'link' => 'https://tablericons.com/',
+                    'comment' => 'lorem ipsum dolorem',
+                    'date_create' => '15.12.2021',
+                    'author' => 'Петя Петров'
+                ]]
+            // тестовые данные
         ]);
     }
 
     public function get()
     {
         // TODO: Implement get() method.
+
     }
 
+    // временно для таблицы
+    private function setTable()
+    {
+        return [
+            [
+                'name' => 'active',
+                'header' => '',
+                'type' => 'bool',
+                'filter' => false,
+                'show' => true,
+                'sort' => false
+            ],
+            [
+                'name' => 'id',
+                'header' => 'ID',
+                'type' => 'int',
+                'filter' => true,
+                'show' => true,
+                'sort' => false
+            ],
+            [
+                'name' => 'name',
+                'header' => 'Название шаблона',
+                'type' => 'string',
+                'filter' => true,
+                'show' => true,
+                'sort' => false
+            ],
+            [
+                'name' => 'link',
+                'header' => 'Ссылка на скачивание',
+                'type' => 'string',
+                'filter' => false,
+                'show' => true,
+                'sort' => false
+            ],
+            [
+                'name' => 'comment',
+                'header' => 'Комментарий',
+                'type' => 'string',
+                'filter' => true,
+                'show' => true,
+                'sort' => false
+            ],
+            [
+                'name' => 'date_create',
+                'header' => 'Дата загрузки',
+                'type' => 'date',
+                'filter' => true,
+                'show' => true,
+                'sort' => false
+            ],
+            [
+                'name' => 'author',
+                'header' => 'Кто разместил',
+                'type' => 'string',
+                'filter' => true,
+                'show' => true,
+                'sort' => false
+            ],
+        ];
+    }
+    // временно для таблицы
 }
