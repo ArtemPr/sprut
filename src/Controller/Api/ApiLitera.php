2888f53a544985faa8b21fb641449128cb9b7812
admhome <admhome@ya.ru>
2022-07-26 17:18:45 +0300

    SPR-144 start of api for litera interface

--- src/Controller/Api/ApiLitera.php
+++ src/Controller/Api/ApiLitera.php
@@ -5,6 +5,7 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Discipline;
 use App\Repository\LiteraRepository;
 use App\Service\ApiService;
 use App\Service\Litera5API;
@@ -13,6 +14,7 @@ use App\Service\UploadedFilesService;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class ApiLitera extends AbstractController
 {
@@ -29,11 +31,36 @@ class ApiLitera extends AbstractController
 
     public function add(): JsonResponse
     {
+        $request = new Request($_GET, $_POST, [], $_COOKIE, $_FILES, $_SERVER);
+        $data = $request->request->all();
+
+        $discipline = null;
+
+        if (empty($data['unique_discipline']) && !empty($data['discipline'])) {
+            $discipline = $this->doctrine->getRepository(Discipline::class)->find($data['discipline']);
+        }
+
         //
+
+        $this->logAction(
+            'add_litera',
+            'Литера5',
+            'Создание запроса '.$lastId
+        );
+
+        return $this->json(['result' => 'success', 'id' => $lastId]);
     }
 
     public function update(): JsonResponse
     {
         //
+
+        $this->logAction(
+            'update_litera',
+            'Литера5',
+            'Создание запроса '.$lastId
+        );
+
+        return $this->json(['result' => 'success', 'id' => $lastId]);
     }
 }
